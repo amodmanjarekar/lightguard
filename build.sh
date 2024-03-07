04669e9b0dbfbadbd91345e5ea69760fb41f23a5
arhantalwar <arhantalwar@gmail.com>
2024-03-08 01:52:42 +0530

    Updating build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=./rust/src/
+LIB_PATH=$(pwd)
+export LD_LIBRARY_PATH=$LIB_PATH
 rustc --crate-type cdylib ./rust/src/helper.rs
 gcc main.c -o main -L. -lhelper -lraylib
