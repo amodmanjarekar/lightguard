6af24f934e6c07b1315defc7d3978d3b4e6e5748
arhantalwar <arhantalwar@gmail.com>
2024-03-07 07:29:08 +0530

    Update: create_polygon

--- main.c
+++ main.c
@@ -1,24 +1,57 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <raylib.h>
 #include "raycast.c"
+#include "points.c"
 
 #define height 1024
 #define width 720
+#define pointsA 7
 
-int main()
-{
+int main() {
+
+    srand(time(NULL));
+
+    Vector2 points_array[pointsA];
+
+    bool is_not_unique = true;
+
+    for (int t = 0; t < pointsA; t++) {
+
+        is_not_unique = true;
+
+        while (is_not_unique) {
+
+            int random_x = rand() % (height);
+            int random_y = rand() % (width);
+
+            for (int i = 0; i < pointsA; i++) {
+
+                if (points_array[i].x == random_x && points_array[i].y == random_y) {
+                    continue;
+                } else {
+                    points_array[t] = (Vector2){ .x = random_x, .y = random_y };
+                    is_not_unique = false;
+                    break;
+                }
+
+            }
+
+        }
+
+    }
 
     SetTraceLogLevel(LOG_WARNING);
+    SetTargetFPS(30);
     InitWindow(height, width, "HELLO");
-    SetTargetFPS(12);
 
     int guardX, guardY;
     bool isLocationSelected = false;
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
 
         BeginDrawing();
-
         ClearBackground(BLACK);
 
         Vector2 points[3] = {
@@ -26,6 +59,12 @@ int main()
             (Vector2){.x = 500, .y = 300},
             (Vector2){.x = 200, .y = 500},
         };
+
+        for (int t = 0; t < pointsA; t++) {
+            DrawLine(points_array[t].x, points_array[t].y, points_array[t+1].x, points_array[t+1].y, RED);
+        }
+
+        EndDrawing();
 
         for (int i = 0; i < 3; i++)
         {
